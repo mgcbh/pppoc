f6f4db01ff04e0255990c30e83b97009da46e835
Michael Girgis <mgirgis@crateandbarrel.com>
2025-03-03 16:25:51 -0600

    Paypal updates.

--- src/features/payment/PayPal.js
+++ src/features/payment/PayPal.js
@@ -5,7 +5,6 @@ import { AdyenCheckout, PayPal } from "@adyen/adyen-web";
 import "@adyen/adyen-web/styles/adyen.css";
 import { initiateCheckout } from "../../app/paymentSlice";
 import Messages from "../../components/Messages";
-import { getRedirectUrl } from "../../util/redirect";
 
 export const PayPalContainer = () => {
   return (
@@ -22,13 +21,19 @@ const Checkout = () => {
   const payment = useSelector(state => state.payment);
   const navigate = useNavigate();
   const amountRef = useRef(null);
+  const checkout = useRef(null);
   const paymentsAmountRef = useRef(null);
   const payPalRef = useRef(null);
   const paymentContainer = useRef(null);
   const [errorMsg, setErrorMsg] = useState('');
-  const [messageResponse, setMessageResponse] = useState('');
-  const [jsonResponse, setJsonResponse] = useState({});
+  const [submitmessage, setSubmitMessage] = useState('');
+  const [submitJson, setSubmitJson] = useState({});
+  const [detailsMessage, setDetailsMessage] = useState('');
+  const [detailsJson, setDetailsJson] = useState({});
   const [initialized, setInitialized] = useState(false);
+  const [ready, setReady] = useState(false);
+  const [intent, setIntent] = useState('capture');
+  const [complete, setComplete] = useState(false);
 
   useEffect(() => {
     dispatch(initiateCheckout());
@@ -43,71 +48,23 @@ const Checkout = () => {
   }, [payment, navigate])
 
   useEffect(() => {
-    console.log('in useffect')
     const { config, paymentMethods } = payment;
+
+    // The 'ignore' flag is used to avoid double re-rendering caused by React 18 StrictMode
+    // More about it here: https://beta.reactjs.org/learn/synchronizing-with-effects#fetching-data
     let ignore = false;
 
-    if (!paymentMethods || !paymentContainer.current || !initialized) {
+    if (!paymentMethods) {
       // initiateCheckout is not finished yet.
       return;
     }
 
     const createCheckout = async () => {
-      const checkout = await AdyenCheckout({
+      checkout.current = await AdyenCheckout({
         ...config,
         paymentMethodsResponse: paymentMethods,
         showPayButton: true,
-        amount: {
-          value: parseInt(amountRef.current.value),
-          currency: "USD"
-        },
-
-        onAdditionalDetails: async (state, component, actions) => {
-          console.log('in onAdditionalDetails');
-          console.log(state)
-          try {
-            // Make a POST /payments/details request from your server.
-            const result = await fetch("/api/payments/details", {
-              method: "POST",
-              body: state.data ? JSON.stringify({
-                ...state.data,
-              }) : "",
-              headers: {
-                "Content-Type": "application/json",
-              }
-            }).then(response => response.json());
-
-            // If the /payments/details request from your server fails, or if an unexpected error occurs.
-            if (!result.resultCode) {
-              actions.reject();
-              return;
-            }
-
-            const {
-              resultCode,
-              action,
-              order,
-              donationToken
-            } = result;
-
-            // If the /payments/details request request form your server is successful, you must call this to resolve whichever of the listed objects are available.
-            // You must call this, even if the result of the payment is unsuccessful.
-            actions.resolve({
-              resultCode,
-              action,
-              order,
-              donationToken,
-            });
-          } catch (error) {
-            console.error("onAdditionalDetails", error);
-            actions.reject();
-          }
-        },
-
-
         onSubmit: async (state, component, actions) => {
-          console.info("onSubmit", state, component, actions);
-          console.log("value: ", parseInt(amountRef.current.value))
           try {
             if (state.isValid) {
               const response = await fetch("/api/payments", {
@@ -123,8 +80,8 @@ const Checkout = () => {
 
               const { action, order, resultCode } = response;
 
-              setMessageResponse('Response from the call to the /payments API:')
-              setJsonResponse(response);
+              setSubmitMessage('Response from the call to the /payments API:')
+              setSubmitJson(response);
 
               if (!resultCode) {
                 console.warn("reject");
@@ -142,9 +99,53 @@ const Checkout = () => {
             actions.reject();
           }
         },
+        onAdditionalDetails: async (state, component, actions) => {
+          try {
+            // Make a POST /payments/details request from your server.
+            const response = await fetch("/api/payments/details", {
+              method: "POST",
+              body: state.data ? JSON.stringify({
+                ...state.data,
+              }) : "",
+              headers: {
+                "Content-Type": "application/json",
+              }
+            }).then(response => response.json());
+
+            setDetailsMessage('Response from the call to the /payments/details API:')
+            setDetailsJson(response);
+
+            // If the /payments/details request from your server fails, or if an unexpected error occurs.
+            if (!response.resultCode) {
+              actions.reject();
+              return;
+            }
+
+            const {
+              resultCode,
+              action,
+              order,
+              donationToken
+            } = response;
+
+            // If the /payments/details request request form your server is successful, you must call this to resolve whichever of the listed objects are available.
+            // You must call this, even if the result of the payment is unsuccessful.
+            actions.resolve({
+              resultCode,
+              action,
+              order,
+              donationToken,
+            });
+          } catch (error) {
+            console.error("onAdditionalDetails", error);
+            actions.reject();
+          }
+        },
         onPaymentCompleted: (result, component) => {
           console.info("onPaymentCompleted", result, component);
-          navigate(getRedirectUrl(result.resultCode), { replace: true });
+          component.unmount();
+          setComplete(true);
+          // navigate(getRedirectUrl(result.resultCode), { replace: true });
         },
         onPaymentFailed: (result, component) => {
           console.info("onPaymentFailed", result, component);
@@ -152,39 +153,12 @@ const Checkout = () => {
         },
         onError: (error, component) => {
           console.error("onError", error.name, error.message, error.stack, component);
+          setErrorMsg(error.message);
           // navigate(`/status/error?reason=${error.message}`, { replace: true });
         },
       })
 
-      console.log('checkout: ', checkout)
-
-      // The 'ignore' flag is used to avoid double re-rendering caused by React 18 StrictMode
-      // More about it here: https://beta.reactjs.org/learn/synchronizing-with-effects#fetching-data
-      if (paymentContainer.current && !ignore) {
-        if (payPalRef.current === null) {
-          const payPalConfiguration = {
-            // intent: "authorize",
-            environment: 'test',
-            countryCode: "US",
-          }
-
-          payPalRef.current = new PayPal(checkout, payPalConfiguration);
-
-          console.log('mounting')
-          payPalRef.current.mount(paymentContainer.current);
-
-          // payPalRef.current
-          //   .isAvailable()
-          //   .then(() => {
-          //     // Mount the PayPal component.
-
-          //   })
-          //   .catch(error => {
-          //     setErrorMsg(error.toString());
-          //     console.log('PayPas is not available: ', error)
-          //   });
-        }
-      }
+      setReady(true);
     }
 
     createCheckout();
@@ -192,46 +166,91 @@ const Checkout = () => {
     return () => {
       ignore = true;
     }
-  }, [payment, navigate, initialized])
+  }, [payment, navigate, setReady])
+
+  const createComponent = () => {
+    if (paymentContainer.current && checkout.current) {
+      if (payPalRef.current === null) {
+        const payPalConfiguration = {
+          intent: intent,
+          environment: 'test',
+          countryCode: "US",
+          amount: {
+            value: parseInt(amountRef.current.value),
+            currency: "USD"
+          },
+        }
+
+        payPalRef.current = new PayPal(checkout.current, payPalConfiguration);
+        payPalRef.current.mount(paymentContainer.current);
+        setInitialized(true);
+      }
+    }
+  }
+
+  const resetComponent = () => {
+    payPalRef.current.unmount();
+    payPalRef.current = null;
+    setInitialized(false);
+    setSubmitJson({});
+    setSubmitMessage('');
+    setDetailsJson({});
+    setDetailsMessage('');
+    setComplete(false);
+    setErrorMsg('');
+  }
 
   return (
-    <div>
-      <p className="red">WORK IN PROGRESS</p>
+    <div className="mw-100">
+      <div className="form-group">
+        <label>Amount:</label>
+        <input disabled={initialized} className="form-control" type="number" ref={amountRef} defaultValue={100} />
+      </div>
 
       <div className="form-group">
-        <label>Amount to send to PayPal and the /payments API:</label>
-        <input className="form-control" type="number" ref={amountRef} defaultValue={100} />
+        <label>Set the intent:</label>
+        <select disabled={initialized} className="form-control" onChange={(event) => setIntent(event.target.value)}>
+          <option value="capture">capture</option>
+          <option value="authorize">authorize</option>
+          <option value="subscription">subscription</option>
+          <option value="tokenize">tokenize</option>
+        </select>
       </div>
 
-
-      {/* <div className="form-group">
-        <label>Amount to send to /payments API:</label>
-        <input className="form-control" type="number" ref={paymentsAmountRef} defaultValue={100} />
-      </div> */}
-
-      {/* <p>The two amount above need to match in order for the payment to succeed. Note that "100" equals "$1.00".</p> */}
-
-      <div className="my-3">
-        <button onClick={() => setInitialized(true)} className="button">Click to initialize PayPal after entering amount.</button>
-      </div>
-
-      {initialized &&
-        <div className="payment-container mb-5">
-          <div ref={paymentContainer} className="payment"></div>
-          {errorMsg && (
-            <div className="p-4">
-              <p>PayPal could not be enabled.</p>
-              <p>Error message: {errorMsg}</p>
-            </div>
-          )}
+      {ready && !initialized &&
+        <div className="my-3">
+          <button onClick={createComponent} className="button">Initialize component</button>
         </div>
       }
 
-      <div className="mb-3">
-        {(messageResponse && jsonResponse) && (
-          <Messages message={messageResponse} json={jsonResponse} />
+      <div className={`${initialized ? 'payment-container mb-5' : 'test'}`}>
+        <div ref={paymentContainer} className="payment" />
+        {complete && <p className="px-3 my-0">Processing complete.</p>}
+        {errorMsg && (
+          <div className="p-4">
+            <p>PayPal could not be enabled.</p>
+            <p>Error message: {errorMsg}</p>
+          </div>
         )}
       </div>
+
+      {(submitmessage && submitJson) && (
+        <div className="mb-3">
+          <Messages message={submitmessage} json={submitJson} />
+        </div>
+      )}
+
+      {(detailsMessage && detailsJson) && (
+        <div className="mb-3">
+          <Messages message={detailsMessage} json={detailsJson} />
+        </div>
+      )}
+
+      {initialized &&
+        <div className="my-3">
+          <button onClick={resetComponent} className="button">Reset component</button>
+        </div>
+      }
     </div>
   );
 }
